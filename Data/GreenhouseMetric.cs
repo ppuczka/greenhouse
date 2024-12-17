dc54f297ecd27c9caf381ace63dd4d7ca66d2dc6
Przemek <p.puczka@gmail.com>
2024-12-17 21:02:30 +0100

    parse CosmosDb items

--- Data/GreenhouseMetric.cs
+++ Data/GreenhouseMetric.cs
@@ -5,33 +5,34 @@ namespace Greenhouse.Data;
 
 public class GreenhouseMetric
 {
-    public int Id { get; set; }
+    public Guid Id { get; set; }
     public int Temperature { get; set; }
     public int Humidity { get; set; }
     public int SoilMoisture { get; set; }
     public HumidityLevel HumidityLevel { get; set; }
     public TemperatureLevel TemperatureLevel { get; set; }
     public SoilMoistureLevel SoilMoistureLevel { get; set; }
+    
     public DateTime DateTime { get; set; }
 }
 
 public enum HumidityLevel
 {
-    High = 0,
-    Optimal = 1,
-    Low = 2,
+    high = 0,
+    optimal = 1,
+    low = 2,
 }
 
 public enum TemperatureLevel
 {
-    High = 0,
-    Optimal = 1,
-    Low = 2,
+    high = 0,
+    optimal = 1,
+    low = 2,
 }
 
 public enum SoilMoistureLevel
 {
-    High = 0,
-    Optimal = 1,
-    Low = 2,
+    dry = 0,
+    moist = 1,
+    wet = 2,
 }
--- Data/MetricsContext.cs
+++ Data/MetricsContext.cs
@@ -6,17 +6,44 @@ namespace Greenhouse.Data;
 
 public class MetricsContext : DbContext
 {
-    // private readonly Config.Config _config;
     public required DbSet<GreenhouseMetric> GreenhouseMetrics { get; set; }
+
     public MetricsContext(DbContextOptions<MetricsContext> options) : base(options) =>
         Debug.WriteLine($"context created.");
 
-    // protected MetricsContext(Config.Config config, DbSet<GreenhouseMetric> greenhouseMetrics)
-    // {
-    //     this._config = config;
-    //     GreenhouseMetrics = greenhouseMetrics;
-    // }
-    //
-    // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    //     => optionsBuilder.UseCosmos(_config.DatabaseEndpoint, new DefaultAzureCredential(), _config.DatabaseName);
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultContainer("greenhouse");
+        modelBuilder.Entity<GreenhouseMetric>().HasNoDiscriminator();
+
+        modelBuilder.Entity<GreenhouseMetric>().Property(g => g.Humidity).ToJsonProperty("humidity");
+        modelBuilder
+            .Entity<GreenhouseMetric>()
+            .Property(g => g.HumidityLevel)
+            .HasConversion(
+                v => v.ToString(),
+                v => (HumidityLevel)Enum.Parse(typeof(HumidityLevel), v))
+            .ToJsonProperty("humidity_level");  
+        
+        modelBuilder.Entity<GreenhouseMetric>().Property(g => g.SoilMoisture).ToJsonProperty("soil_moisture");
+        modelBuilder
+            .Entity<GreenhouseMetric>()
+            .Property(g => g.SoilMoistureLevel)
+            .HasConversion(
+                v => v.ToString(),
+                v => (SoilMoistureLevel)Enum.Parse(typeof(SoilMoistureLevel), v))
+            .ToJsonProperty("soil_moisture_level");
+            
+        modelBuilder.Entity<GreenhouseMetric>().Property(g => g.Temperature).ToJsonProperty("temperature");
+        modelBuilder
+            .Entity<GreenhouseMetric>()
+            .Property(g => g.TemperatureLevel)
+            .HasConversion(
+                v => v.ToString(),
+                v => (TemperatureLevel)Enum.Parse(typeof(TemperatureLevel), v))
+            .ToJsonProperty("temperature_level");
+        
+        modelBuilder.Entity<GreenhouseMetric>().Property(g => g.DateTime).ToJsonProperty("date_time");
+
+    }
 }
