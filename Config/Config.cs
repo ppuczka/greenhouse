0fcceb09f6acb88f66d31fa9dfbe24119266790e
Przemek <p.puczka@gmail.com>
2025-01-15 22:24:34 +0100

    warning fix add bicep sample for configuring docker web-app

--- Config/Config.cs
+++ Config/Config.cs
@@ -2,8 +2,8 @@ namespace Greenhouse.Config;
 
 public class Config
 {
-    public string AppName { get; }
-    public string AppVersion { get; }
-    public string DatabaseEndpoint { get; }
-    public string DatabaseName { get; }
+    public string? AppName { get; }
+    public string? AppVersion { get; }
+    public string? DatabaseEndpoint { get; }
+    public string? DatabaseName { get; }
 }
